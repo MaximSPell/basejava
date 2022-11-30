3a883a70a9671e3886ad2c83bf4ee9d69c9f9b51
MaximSPell <qwer_max_777@mail.ru>
2022-11-30 15:14:45 +0300

    HW2 30.11.2022 15:14

--- src/MainArray.java
+++ src/MainArray.java
@@ -1,13 +1,17 @@
+import model.Resume;
+import storage.ArrayStorage;
+import storage.Storage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Interactive test for ArrayStorage implementation
+ * Interactive test for storage.ArrayStorage implementation
  * (just run, no need to understand)
  */
 public class MainArray {
-    private final static ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private final static Storage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -29,7 +33,7 @@ public class MainArray {
                     break;
                 case "update":
                     resume = new Resume();
-                    resume.uuid = uuid;
+                    resume.setUuid(uuid);
                     ARRAY_STORAGE.update(resume);
                     printAll();
                     break;
@@ -38,7 +42,7 @@ public class MainArray {
                     break;
                 case "save":
                     resume = new Resume();
-                    resume.uuid = uuid;
+                    resume.setUuid(uuid);
                     ARRAY_STORAGE.save(resume);
                     printAll();
                     break;
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -1,31 +1,31 @@
+import model.Resume;
+import storage.ArrayStorage;
+
 /**
- * Test for your ArrayStorage implementation
+ * Test for your storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
         Resume resumeFirst = new Resume();
-        resumeFirst.uuid = "uuid1";
+        resumeFirst.setUuid("uuid1");
         Resume resumeSecond = new Resume();
-        resumeSecond.uuid = "uuid2";
+        resumeSecond.setUuid("uuid2");
         Resume resumeThird = new Resume();
-        resumeThird.uuid = "uuid3";
-        Resume resumeTestUpdate = new Resume();
-        resumeTestUpdate.uuid = "uuid3";
+        resumeThird.setUuid("uuid3");
 
         ARRAY_STORAGE.save(resumeFirst);
         ARRAY_STORAGE.save(resumeSecond);
         ARRAY_STORAGE.save(resumeThird);
-        ARRAY_STORAGE.update(resumeTestUpdate);
 
-        System.out.println("Get r1: " + ARRAY_STORAGE.get(resumeFirst.uuid));
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(resumeFirst.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
-        ARRAY_STORAGE.delete(resumeFirst.uuid);
+        ARRAY_STORAGE.delete(resumeFirst.getUuid());
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
